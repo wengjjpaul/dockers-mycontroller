018982f512c28771e0cedd6de065f196cdebd7a1
paulweng <wengjjpaul@gmail.com>
2017-01-20 21:02:56 +0800

    - added config

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ docker run -dti \
  -p 1883:1883 \
  -p 9001:9001 \
  --restart='always' \
- -v /home/paul/mycontroller/backup:/data/mycontroller/backup \
+ -v /home/paul/mycontroller/dockers-rpi-mycontroller/backup:/data/mycontroller/backup \
+ -v /home/paul/mycontroller/dockers-rpi-mycontroller/conf:/data/mycontroller/conf \
  --name mycontroller scino/mycontroller \
- /bin/bash -c "./mycontroller/bin/start.sh; /bin/bash"
+ /bin/bash -c "/data/mycontroller/bin/start.sh; /bin/bash"
